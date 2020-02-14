557e2829ed1563705b4fe5b3765dc4b18e8753e9
Richard Medina <ricki9@gmail.com>
2020-02-13 23:49:30 -0600

    Added docker file

--- mca-learnow-api/Controllers/WeatherForecastController.cs
+++ mca-learnow-api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace mca_learnow_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Primero", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -27,7 +27,7 @@ namespace mca_learnow_api.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(0, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
